3e6950ad54fced96dc4cd2027a1fec3fc9719374
EduardRytslin <136772968+EduardRytslin@users.noreply.github.com>
2025-04-25 23:11:59 +0300

    Update Seminar1_4.java

--- Seminar1_4.java
+++ Seminar1_4.java
@@ -9,5 +9,27 @@
 “подсветить”
  */
 public class Seminar1_4 {
-    
+
+public static void main(String[] args){
+Integer[] arr = {1, 2, 2, 4, 3};
+List<Integer> nullElements checkArray(arr);
+if (nullElements.size() > 0){
+    System.out.println("Массив содержит пустые элементы на позициях" nullElements);
+    }
+else {
+    System.out.println("Массив не содержит пустых значений");
+    }
+}
+
+public static List<Integer> checkArray (Integer[] arr){
+List<Integer> nullElements new ArrayList<>();
+for (int i = 0; i < arr.length; i++){
+    if (arr[1] == null){
+    nullElements.add(i);
+        }
+    }
+return nullElements;
+}
+
+}
 }
