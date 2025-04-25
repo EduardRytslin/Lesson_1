2ef02cdc6675b743802ade5af1fbe660f7af7844
EduardRytslin <136772968+EduardRytslin@users.noreply.github.com>
2025-04-25 21:45:42 +0300

    Add files via upload

---
+++ Seminar1_5.java
@@ -0,0 +1,4 @@
+
+public class Seminar1_5 {
+    
+}
---
+++ Seminar1_6.java
@@ -0,0 +1,7 @@
+/*
+Дан класс: https://github.com/sabinaIsm/test/blob/d2ebf811b85139783b1a0dcbe3714dbb28deee82/RandomExceptionsClass.java
+Запустите код, с помощью анализа стектрейса найдите и по возможности исправьте ошибки, добейтесь корректного выполнения программы.
+ */
+public class Seminar1_6 {
+    
+}
